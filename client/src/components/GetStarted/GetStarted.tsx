2f8a3cd4f77dbb9824775b1a8d32fbce4f267061
Benjamin Huy Pham <benjaminhpham@gmail.com>
2023-08-22 14:48:44 -0700

    implemented and styled getStarted section

---
+++ client/src/components/GetStarted/GetStarted.tsx
@@ -0,0 +1,21 @@
+import "./GetStarted.css";
+
+export default function GetStarted() {
+  return (
+    <section className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get Started with Homyz</span>
+          <span className="secondaryText">
+            Subscribe and Find Super Attractive Price Quotes from
+            <br />
+            Find Your Residence Soon
+          </span>
+          <button className="button">
+            <a href="mailto:benji@gmail.com">Get Started</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
